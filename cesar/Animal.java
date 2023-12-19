bab21ae36c87c397769ca3c807da5a4cbf22acbb
flarom <151688146+flarom@users.noreply.github.com>
2023-12-19 23:04:40 +0000

    Separei todas as classes em arquivos separados

---
+++ cesar/Animal.java
@@ -0,0 +1,20 @@
+abstract class Animal{
+    String som;
+    String nome;
+    String raca;
+    String cor;
+    int idade;
+
+    public void atraphalharSono(){
+        for (int i = 0; i < 3; i++) {
+            System.out.println(this.som);
+        }
+    }
+    public String toString(){ // representa a classe atual em forma de string
+        return "Nome: " + this.nome + "\n" +
+            "Raca: " + this.raca + "\n" +
+            "Cor: " + this.cor + "\n" +
+            "Idade: " + this.idade + " anos \n" +
+            "Onomatopeia: " + this.som + "\n";
+    }
+}
---
+++ cesar/Cavalo.java
@@ -0,0 +1,9 @@
+class Cavalo extends Animal{
+    public Cavalo(String nome, String raca, String cor, int idade){
+        this.som = "ihohoho";
+        this.nome = nome;
+        this.raca = raca;
+        this.cor = cor;
+        this.idade = idade;
+    }
+}
