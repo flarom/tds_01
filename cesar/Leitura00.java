d4253b0e568352adb9de8c5ebf73ea39339636dd
flarom <151688146+flarom@users.noreply.github.com>
2023-12-20 00:38:08 +0000

    Criada nova classe de leitura, que usa leitura dinâmica para dar valores ao cachorro

---
+++ cesar/Leitura00.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Leitura00{
+    public static void main(String [] args){
+        Scanner s = new Scanner(System.in);
+        String nome;
+        String raca;
+        String cor;
+        Integer idade;
+
+        System.out.println("Entre o nome do cachorro: ");
+        nome = s.nextLine();
+        System.out.println("Entre a raça do cachorro: ");
+        raca = s.nextLine();
+        System.out.println("Entre a cor do cachorro: ");
+        cor = s.nextLine();
+        System.out.println("Entre a idade do cachorro: ");
+        idade = s.nextInt();
+
+        Cachorro cao1 = new Cachorro(nome, raca, cor, idade);
+        cao1.atraphalharSono();
+        System.out.println(cao1);
+    }
+}
