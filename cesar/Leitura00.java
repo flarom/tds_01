4c23af687529aa3ecf278f1dc724db11a0085073
flarom <151688146+flarom@users.noreply.github.com>
2023-12-20 00:47:02 +0000

    arrumei a formatação, tava feio

--- cesar/Leitura00.java
+++ cesar/Leitura00.java
@@ -8,11 +8,11 @@ public class Leitura00{
         String cor;
         Integer idade;
 
-        System.out.println("Entre o nome do cachorro: ");
+        System.out.println("Entre o nome do cachorro : ");
         nome = s.nextLine();
-        System.out.println("Entre a raça do cachorro: ");
+        System.out.println("Entre a raça do cachorro : ");
         raca = s.nextLine();
-        System.out.println("Entre a cor do cachorro: ");
+        System.out.println("Entre a cor do cachorro  : ");
         cor = s.nextLine();
         System.out.println("Entre a idade do cachorro: ");
         idade = s.nextInt();
